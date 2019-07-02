d94afd9da01f9f89a683665ce0fc183345497719
Renato Fabbri <renato.fabbri@gmail.com>
2019-07-01 23:30:22 -0300

    added test script

---
+++ tests/test.py
@@ -0,0 +1,125 @@
+import losd as l
+pl = l.plainQueryValues
+q = l.query
+
+# get all snapshots:
+query = '''
+SELECT ?s WHERE {
+  ?s a po:Snapshot
+}
+'''
+
+res = pl(q(query))
+
+##########
+# from here on, check to assure that the uris correspond to
+# the snapshot (types) intended
+
+# get all friendship relations in a facebook snapshot:
+uri = res[99]
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?f a po:Friendship . ?f po:snapshot <%s> .
+?f po:member ?a1, ?a2 .
+FILTER(?a1 != ?a2)
+}
+''' % (uri,)
+res2 = pl(q(query))
+
+
+# get all retweet interactions in a Twitter snapshot:
+uri = res[-1]
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?m1 po:retweetOf ?m2 . ?m1 po:author ?a1 . ?m2 po:author ?a2 .
+?m1 po:snapshot <%s>
+}
+''' % (uri,)
+res3 = pl(q(query))
+
+# get all interactions in a email snapshot:
+uri = res[48]
+query = '''
+SELECT ?from ?to WHERE {
+?message1 po:snapshot <%s> . ?message2 po:replyTo ?message1 .
+?message1 po:author ?from . ?message2 po:author ?to .
+}
+''' % (uri,)
+res4 = pl(q(query))
+
+# get all interactions in a IRC snapshot, with texts:
+uri = res[102]
+query = '''
+SELECT ?a1 ?a2 ?t WHERE {
+?m a po:IRCMessage . ?m po:author ?a1 . ?m po:directedTo ?a2 .
+?m po:cleanText ?t . ?m po:snapshot <%s>
+}
+''' % (uri,)
+res5 = pl(q(query))
+
+# get all interactions in the AA snapshot:
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?s po:author ?a1 . ?s po:checkParticipant ?a2 .
+}
+'''
+res6 = pl(q(query))
+
+
+# get all friendshipts in the Participa.BR snapshot:
+uri = res[104]
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?f a po:Friendship . ?f po:snapshot <%s> .
+?f po:member ?a1, ?a2 .
+FILTER(?a1 != ?a2)
+}
+''' % (uri,)
+res7 = pl(q(query))
+
+# get all interactions in the Participa.BR snapshot:
+uri = res[104]
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?a po:snapshot <%s> . ?a a po:Article .
+?a po:author ?a1 . ?c po:article ?a . ?c po:author ?a2 .
+}
+''' % (uri,)
+res8 = pl(q(query))
+
+# get all interaction in the Cidade Democrática snapshot:
+uri = res[45]
+query = '''
+SELECT ?a1 ?a2 WHERE {
+?t po:snapshot <%s> .  ?t a po:Topic . ?t po:author ?a1 .
+?c a po:Comment . ?c po:topic ?t . ?c po:author ?a2 .
+}
+''' % (uri,)
+res9 = pl(q(query))
+
+
+interaction = [
+    res3,
+    res4,
+    res5,
+    res6,
+    res8,
+    res9
+]
+friendship = [
+    res2,
+    res7,
+]
+
+fr = []
+for relations in friendship:
+    fr.append(l.mkRelationNetwork(relations))
+
+inte = []
+for interactions in interaction:
+    inte.append(l.mkInteractionNetwork(interactions))
+
+# finished. Play with the networks in fr and inte
+# maybe use networkx and pylab to plat them
+
+# :::
